d6bcc2c2752367adfe280dfe5db12dd4f5e0faa9
Saman Shahzadi <samanshahzadi555@gmail.com>
2026-08-11 00:55:17 +0500

    Added .NET Backend API

---
+++ backend/Ticketwise.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticketwise.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+
+        public OkResult GetHealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
